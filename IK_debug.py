559fa1cf4dce1b9ab9ec7d3d23eb8241c2eb180c
Jeremy Hale <jeremy.hale@gmail.com>
2018-06-14 17:36:59 -0700

    ...

--- IK_debug.py
+++ IK_debug.py
@@ -2,6 +2,7 @@ from sympy import *
 from time import time
 from mpmath import radians
 import tf
+import numpy as np
 
 '''
 Format of test case is [ [[EE position],[EE orientation as quaternions]],[WC location],[joint angles]]
@@ -58,34 +59,142 @@ def test_code(test_case):
 
     req = Pose(comb)
     start_time = time()
-    
-    ########################################################################################
-    ## 
 
-    ## Insert IK code here!
-    
-    theta1 = 0
-    theta2 = 0
-    theta3 = 0
-    theta4 = 0
-    theta5 = 0
-    theta6 = 0
-
-    ## 
     ########################################################################################
-    
+    ##
+    # Step 1: DH parameters
+    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+
+    # Arm length constants
+    a1 = 0.35
+    d1 = 0.33 + 0.42
+    a2 = 1.25
+    a3 = -0.054
+    d4 = 0.96 + 0.54
+    d7 = 0.193 + 0.11
+
+    dh_table = {alpha0: 0, a0: 0, d1: d1, q1: q1,
+                alpha1: -pi/2, a1: a1, d2: 0, q2: q2 - pi/2,
+                alpha2: 0, a2: a2, d3: 0, q3: q3,
+                alpha3: -pi/2, a3: a3, d4: d4, q4: q4,
+                alpha4: pi/2, a4: 0, d5: 0, q5: q5,
+                alpha5: -pi/2, a5: 0, d6: 0, q6: q6,
+                alpha6: 0, a6: 0, d7: d7, q7: 0}
+
+    def DH_Transform(alpha, a, d, theta):
+        return Matrix(
+            [[cos(theta), -sin(theta), 0, a],
+            [sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -sin(alpha) * d],
+            [sin(theta)*sin(alpha), cos(theta)*sin(alpha), cos(alpha), cos(alpha) * d],
+            [0, 0, 0, 1]])
+
+    t0_1 = DH_Transform(alpha0, a0, d1, q1).subs(dh_table)
+    t1_2 = DH_Transform(alpha1, a1, d2, q2).subs(dh_table)
+    t2_3 = DH_Transform(alpha2, a2, d3, q3).subs(dh_table)
+    t3_4 = DH_Transform(alpha3, a3, d4, q4).subs(dh_table)
+    t4_5 = DH_Transform(alpha4, a4, d5, q5).subs(dh_table)
+    t5_6 = DH_Transform(alpha5, a5, d6, q6).subs(dh_table)
+    t6_7 = DH_Transform(alpha6, a6, d7, q7).subs(dh_table)
+
+# Step 2: find Wrist Centre
+    t0_ee = t0_1 * t1_2 * t2_3 * t3_4 * t4_5 * t5_6 * t6_7
+
+    r, p, y = symbols('r p y')
+    rotation_x = Matrix([
+        [1, 0, 0],
+        [0, cos(r), -sin(r)],
+        [0, sin(r), cos(r)]])
+    rotation_y = Matrix([
+        [cos(p), 0, sin(p)],
+        [0, 1, 0],
+        [-sin(p), 0, cos(p)]])
+    rotation_z = Matrix([
+        [cos(y), -sin(y), 0],
+        [sin(y), cos(y), 0],
+        [0, 0, 1]])
+
+    rotation_intrinsic = rotation_z * rotation_y * rotation_x
+    rotation_correction = rotation_z.subs(y, pi) * rotation_y.subs(p, -pi/2)
+    roll, pitch, yaw = tf.transformations.euler_from_quaternion(
+        [req.poses[0].orientation.x,
+        req.poses[0].orientation.y,
+        req.poses[0].orientation.z,
+        req.poses[0].orientation.w])
+
+    rotation_ee = (rotation_intrinsic * rotation_correction).subs({'r': roll, 'p': pitch, 'y': yaw})
+
+    px, py, pz = req.poses[0].position.x, req.poses[0].position.y, req.poses[0].position.z
+    end_effector = Matrix([[px], [py], [pz]])
+
+    # translate along z-axis (See lesson 11.18 Inverse Kinematics)
+    wrist_centre = end_effector - d7 * rotation_ee[:,2]
+
+    print 'Calculated WC is {}, test case centre is {}'.format(wrist_centre, test_case[1])
+
+    # Step 3: find first 3 join angles
+
+    theta1 = atan2(wrist_centre[1], wrist_centre[0])
+    print 'theta1 {} type {}'.format(theta1, type(theta1))
+
+    # SSS and law of cosines
+    # side c is simple
+    side_c = a2
+    print 'side C {}'.format(side_c)
+    # determine fixed length from joint 3 to joint 5
+    side_a = sqrt(d4 ** 2 + a3 ** 2)
+    print 'side A {}'.format(side_a)
+    # determine length from joint 2 to wrist centre
+    side_b = sqrt(
+        (sqrt(wrist_centre[0] ** 2 + wrist_centre[1] ** 2) - a1) ** 2 +
+        (wrist_centre[2] - d1) ** 2)
+    print 'side B {}'.format(side_b)
+
+    angle_a = acos((side_b ** 2 + side_c ** 2 - side_a ** 2) / (2 * side_b * side_c))
+    print 'angle_a {}'.format(angle_a)
+    angle_b = acos((side_a ** 2 + side_c ** 2 - side_b ** 2) / (2 * side_a * side_c))
+#    angle_c = pi - angle_a - angle_b
+    angle_c = acos((side_a ** 2 + side_b ** 2 - side_c ** 2) / (2 * side_a * side_b))
+    print 'theta2 sqrt component {}'.format(sqrt(wrist_centre[0] ** 2 + wrist_centre[1] ** 2) - a1)
+    print 'theta2 arctan component {}'.format(atan2(wrist_centre[2] - d1, sqrt(wrist_centre[0] ** 2 + wrist_centre[1] ** 2) - a1))
+
+    theta2 = pi / 2 - angle_a - atan2(wrist_centre[2] - d1, sqrt(wrist_centre[0] ** 2 + wrist_centre[1] ** 2) - a1)
+    print 'theta2 {} type {}'.format(theta2, type(theta2))
+
+    side_d = sqrt(0.96 ** 2 + 0.054 ** 2)
+    side_e = 0.54
+    theta3_correction  =  atan2(abs(a3), d4)
+    print "theta3 correction {}".format(theta3_correction)
+    theta3 = pi / 2 - angle_b - 0.036
+    print 'theta3 {} type {}'.format(theta3, type(theta3))
+
+    R0_3 = t0_1[0:3, 0:3] * t1_2[0:3, 0:3] * t2_3[0:3,0:3]
+    R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
+    R3_6 = R0_3.transpose() * rotation_ee
+
+    theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+    print 'theta4 {} type {}'.format(theta4, type(theta4))
+    theta5 = atan2(sqrt(R3_6[0,2] ** 2 + R3_6[2,2] ** 2), R3_6[1,2])
+    theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+
+    ##
+    ########################################################################################
+
     ########################################################################################
     ## For additional debugging add your forward kinematics here. Use your previously calculated thetas
     ## as the input and output the position of your end effector as your_ee = [x,y,z]
 
     ## (OPTIONAL) YOUR CODE HERE!
+    FK = t0_ee.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
 
     ## End your code input for forward kinematics here!
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
-    your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
+    your_wc = [wrist_centre[0], wrist_centre[1], wrist_centre[2]] # <--- Load your calculated WC values in this array
+    your_ee = [FK[0,3], FK[1,3], FK[2,3]] # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
 
     ## Error analysis
